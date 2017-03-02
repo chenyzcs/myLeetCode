5e04cccd8ec9a2db70110880359f115aa9ec8805
zhou <zhou@ZHOU-PC>
2017-03-02 13:49:07 +0800

    FizzBuzz2 use to_string to make a 3ms solution.

--- FizzBuzz_412/main.cpp
+++ FizzBuzz_412/main.cpp
@@ -29,6 +29,27 @@ vector<string> fizzBuzz(int n)
 	return _ans;
 }
 
+vector<string> fizzBuzz2(int n)
+{
+	bool _flag3 =false, _flag5 = false, _flag3_5 = false;
+	vector<string> _ans = {};
+	for (size_t i = 1; i < n + 1; i++)
+	{
+		_flag3 = (i % 3 == 0) ? true : false;
+		_flag5 = (i % 5 == 0) ? true : false;
+		if (_flag3 && !_flag5)
+			_ans.push_back("Fizz");
+		else if (_flag5 && !_flag3)
+			_ans.push_back("Buzz");
+		else if (_flag3 && _flag5)
+			_ans.push_back("FizzBuzz");
+		else
+			_ans.push_back(to_string(i));
+	}
+	return _ans;
+}
+
+
 int main()
 {
 	auto ans = fizzBuzz(15);
